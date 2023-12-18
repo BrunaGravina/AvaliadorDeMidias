f41a8d43d55205e47885786e2cf014e28273eec9
BrunaGravina <brunagravina23@outlook.com>
2023-12-18 20:06:15 -0300

    Adding the research for every movie

--- src/br/com/principal/bru/PrincipalWithSearch.java
+++ src/br/com/principal/bru/PrincipalWithSearch.java
@@ -5,16 +5,24 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalWithSearch {
 
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner e = new Scanner(System.in);
+        System.out.println("Digite um filme para a busca");
+        String search = e.nextLine();
+
+        String address = "https://www.omdbapi.com/?t=" + search + "&apikey=855322df";
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?t=hunger+games&apikey=855322df"))
+                .uri(URI.create(address))
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
         System.out.println(response.body());
+
     }
 }
